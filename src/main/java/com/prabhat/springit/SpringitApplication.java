3fcff900db217b255fd87aeee241130f4da0c4e8
prabhatkumarrai <prabhatkumar27@gmail.com>
2021-05-07 18:38:30 +0530

    Created the entities using Lombok

--- src/main/java/com/prabhat/springit/SpringitApplication.java
+++ src/main/java/com/prabhat/springit/SpringitApplication.java
@@ -1,13 +1,16 @@
 package com.prabhat.springit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringitApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(SpringitApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringitApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/prabhat/springit/domain/Comment.java
@@ -0,0 +1,26 @@
+package com.prabhat.springit.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NonNull
+    private String body;
+
+    //Link
+
+
+}
