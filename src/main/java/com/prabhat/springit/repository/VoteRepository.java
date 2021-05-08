b40124b73d042d99e138b4ccbdcd4ffe052edae6
prabhatkumarrai <prabhatkumar27@gmail.com>
2021-05-08 23:48:07 +0530

    Add Repositories

---
+++ src/main/java/com/prabhat/springit/repository/VoteRepository.java
@@ -0,0 +1,7 @@
+package com.prabhat.springit.repository;
+
+import com.prabhat.springit.domain.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+}
