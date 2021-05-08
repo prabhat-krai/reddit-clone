8a5832213cb715d29fd4ddb41830a7f8d51410fa
prabhatkumarrai <prabhatkumar27@gmail.com>
2021-05-08 23:56:41 +0530

    Adding mappings from link to comment

--- src/main/java/com/prabhat/springit/domain/Link.java
+++ src/main/java/com/prabhat/springit/domain/Link.java
@@ -7,6 +7,9 @@ import lombok.NonNull;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
@@ -21,5 +24,8 @@ public class Link {
     @NonNull
     private String url;
     // Comments
+
+    @OneToMany(mappedBy = "link")
+    private List<Comment> comments = new ArrayList<>();
 
 }
