8298135de5125c696f6adcc5c5eaad4cb5945f91
prabhatkumarrai <prabhatkumar27@gmail.com>
2021-05-11 15:12:17 +0530

    Add views for index, show and create

--- src/main/java/com/prabhat/springit/SpringitApplication.java
+++ src/main/java/com/prabhat/springit/SpringitApplication.java
@@ -1,18 +1,12 @@
 package com.prabhat.springit;
 
-import com.prabhat.springit.domain.Comment;
-import com.prabhat.springit.domain.Link;
-import com.prabhat.springit.repository.CommentRepository;
-import com.prabhat.springit.repository.LinkRepository;
+import org.ocpsoft.prettytime.PrettyTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import java.util.List;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -24,19 +18,10 @@ public class SpringitApplication {
 		SpringApplication.run(SpringitApplication.class, args);
 	}
 
+
 	@Bean
-	CommandLineRunner runner(LinkRepository linkRepository, CommentRepository commentRepository) {
-		return args -> {
-			Link link = new Link("Getting Started with Spring boot 2", "www.google.com");
-			linkRepository.save(link);
+	PrettyTime prettyTime() {
+		return new PrettyTime();
+	}
 
-			Comment comment = new Comment("This is awesome", link);
-			commentRepository.save(comment);
-			link.addComment(comment);
-
-			System.out.println("=======================");
-			System.out.println("Data Inserted in database");
-			System.out.println("=======================");
-		};
-	};
 }
--- src/main/java/com/prabhat/springit/domain/Link.java
+++ src/main/java/com/prabhat/springit/domain/Link.java
@@ -1,12 +1,23 @@
 package com.prabhat.springit.domain;
 
+
+import com.prabhat.springit.service.BeanUtil;
 import lombok.*;
+import org.hibernate.validator.constraints.URL;
+import org.ocpsoft.prettytime.PrettyTime;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotEmpty;
+import java.awt.desktop.PreferencesEvent;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -20,8 +31,11 @@ public class Link extends Auditable{
     @GeneratedValue
     private Long id;
     @NonNull
+    @NotEmpty(message = "Please enter a title")
     private String title;
     @NonNull
+    @NotEmpty(message = "Please enter a URL")
+    @URL(message = "Please enter a valid URL")
     private String url;
     // Comments
 
@@ -30,5 +44,20 @@ public class Link extends Auditable{
 
     public void addComment(Comment comment) {
         comments.add(comment);
+    }
+
+    public String getDomainName() throws URISyntaxException {
+        URI uri = new URI(this.url);
+        String domain = uri.getHost();
+        return domain.startsWith("www.") ? domain.substring(4) : domain;
+    }
+
+    public String getPrettyTime() {
+        PrettyTime pt = BeanUtil.getBean(PrettyTime.class);
+        return pt.format(convertToDateViaInstant(getCreationDate()));
+    }
+
+    private Date convertToDateViaInstant(LocalDateTime dateToConvert) {
+        return java.util.Date.from(dateToConvert.atZone(ZoneId.systemDefault()).toInstant());
     }
 }
